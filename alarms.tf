480df8b054f40b9f0200bbf29aee249aab37f292
Angel Lopez <78411884+angellopez-sg@users.noreply.github.com>
2022-04-20 11:24:56 +0200

    Feature/adding metrics (#2)
    * Adding KMS and SNS resources
    
    Signed-off-by: Angel Lopez <angel.lopez@stratusgrid.com>
    
    * Adding metrics from CloudTrail sources
    
    Signed-off-by: Angel Lopez <angel.lopez@stratusgrid.com>
    
    * Adding alarms and metrcis for WAR compliance
    
    Signed-off-by: Angel Lopez <angel.lopez@stratusgrid.com>
    
    * Adding last line in data and variables
    
    Signed-off-by: Angel Lopez <angel.lopez@stratusgrid.com>

---
+++ alarms.tf
@@ -0,0 +1,194 @@
+# Alarm to detect Root account usage
+resource "aws_cloudwatch_metric_alarm" "root_account_usage" {
+  alarm_name          = "${var.name_prefix}-root-account-usage-alarm${var.name_suffix}"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = "1"
+  metric_name         = aws_cloudwatch_log_metric_filter.root_account_usage.metric_transformation[0].name
+  namespace           = aws_cloudwatch_log_metric_filter.root_account_usage.metric_transformation[0].namespace
+  period              = "60"
+  statistic           = "Sum"
+  threshold           = "80"
+  alarm_description   = "This Alarm monitors if the Root account has logged in the AWS Accounts"
+}
+
+# Alarm to detect AWS Console sign-ins without MFA
+resource "aws_cloudwatch_metric_alarm" "console_sign_in_without_mfa" {
+  alarm_name          = "${var.name_prefix}-console-sign-in-without-mfa-alarm${var.name_suffix}"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = "1"
+  metric_name         = aws_cloudwatch_log_metric_filter.console_sign_in_without_mfa.metric_transformation[0].name
+  namespace           = aws_cloudwatch_log_metric_filter.console_sign_in_without_mfa.metric_transformation[0].namespace
+  period              = "60"
+  statistic           = "Sum"
+  threshold           = "80"
+  alarm_description   = "This Alarm monitors if someone has logged in the AWS Console without MFA"
+}
+
+# Alarm to detect AWS authorization failures
+resource "aws_cloudwatch_metric_alarm" "aws_authorization_failures" {
+  alarm_name          = "${var.name_prefix}-aws-authorization-failures-alarm${var.name_suffix}"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = "1"
+  metric_name         = aws_cloudwatch_log_metric_filter.aws_authorization_failures.metric_transformation[0].name
+  namespace           = aws_cloudwatch_log_metric_filter.aws_authorization_failures.metric_transformation[0].namespace
+  period              = "60"
+  statistic           = "Sum"
+  threshold           = "80"
+  alarm_description   = "This Alarm monitors if there are authorization failures in AWS"
+}
+
+# Alarm to detect AWS console sign in failures
+resource "aws_cloudwatch_metric_alarm" "aws_console_sign_in_failures" {
+  alarm_name          = "${var.name_prefix}-aws_console-sign-in-failures-alarm${var.name_suffix}"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = "1"
+  metric_name         = aws_cloudwatch_log_metric_filter.aws_console_sign_in_failures.metric_transformation[0].name
+  namespace           = aws_cloudwatch_log_metric_filter.aws_console_sign_in_failures.metric_transformation[0].namespace
+  period              = "60"
+  statistic           = "Sum"
+  threshold           = "80"
+  alarm_description   = "This Alarm monitors if there are AWS Console sign in failures"
+}
+
+# Alarm to detect IAM auth changes in AWS
+resource "aws_cloudwatch_metric_alarm" "iam_auth_config_changes" {
+  alarm_name          = "${var.name_prefix}-iam-auth-config-changes-alarm${var.name_suffix}"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = "1"
+  metric_name         = aws_cloudwatch_log_metric_filter.iam_auth_config_changes.metric_transformation[0].name
+  namespace           = aws_cloudwatch_log_metric_filter.iam_auth_config_changes.metric_transformation[0].namespace
+  period              = "60"
+  statistic           = "Sum"
+  threshold           = "80"
+  alarm_description   = "This Alarm monitors if there are IAM Auth changes"
+}
+
+# Alarm to detect changes in AWS Organization
+resource "aws_cloudwatch_metric_alarm" "organizations_changes" {
+  alarm_name          = "${var.name_prefix}-iam-auth-config-changes-alarm${var.name_suffix}"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = "1"
+  metric_name         = aws_cloudwatch_log_metric_filter.organizations_changes.metric_transformation[0].name
+  namespace           = aws_cloudwatch_log_metric_filter.organizations_changes.metric_transformation[0].namespace
+  period              = "60"
+  statistic           = "Sum"
+  threshold           = "80"
+  alarm_description   = "This Alarm monitors if there are changes in AWS Organizations"
+}
+
+# Alarm to detect changes in VPC Gateway configuration
+resource "aws_cloudwatch_metric_alarm" "vpc_gateway_config_changes" {
+  alarm_name          = "${var.name_prefix}-vpc-gw-config-changes-alarm${var.name_suffix}"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = "1"
+  metric_name         = aws_cloudwatch_log_metric_filter.vpc_gateway_config_changes.metric_transformation[0].name
+  namespace           = aws_cloudwatch_log_metric_filter.vpc_gateway_config_changes.metric_transformation[0].namespace
+  period              = "60"
+  statistic           = "Sum"
+  threshold           = "80"
+  alarm_description   = "This Alarm monitors if there are changes in VPC Gateway configuration"
+}
+
+# Alarm to detect changes in Security groups
+resource "aws_cloudwatch_metric_alarm" "security_group_config_changes" {
+  alarm_name          = "${var.name_prefix}-sg-config-alarm${var.name_suffix}"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = "1"
+  metric_name         = aws_cloudwatch_log_metric_filter.security_group_config_changes.metric_transformation[0].name
+  namespace           = aws_cloudwatch_log_metric_filter.security_group_config_changes.metric_transformation[0].namespace
+  period              = "60"
+  statistic           = "Sum"
+  threshold           = "80"
+  alarm_description   = "This Alarm monitors if there are changes in Security groups"
+}
+
+# Alarm to detect changes in VPCs
+resource "aws_cloudwatch_metric_alarm" "vpc_network_config_changes" {
+  alarm_name          = "${var.name_prefix}-vpc-config-alarm${var.name_suffix}"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = "1"
+  metric_name         = aws_cloudwatch_log_metric_filter.vpc_network_config_changes.metric_transformation[0].name
+  namespace           = aws_cloudwatch_log_metric_filter.vpc_network_config_changes.metric_transformation[0].namespace
+  period              = "60"
+  statistic           = "Sum"
+  threshold           = "80"
+  alarm_description   = "This Alarm monitors if there are changes in VPC"
+}
+
+# Alarm to detect changes in Route tables
+resource "aws_cloudwatch_metric_alarm" "route_table_config_changes" {
+  alarm_name          = "${var.name_prefix}-route-table-config-changes-alarm${var.name_suffix}"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = "1"
+  metric_name         = aws_cloudwatch_log_metric_filter.route_table_config_changes.metric_transformation[0].name
+  namespace           = aws_cloudwatch_log_metric_filter.route_table_config_changes.metric_transformation[0].namespace
+  period              = "60"
+  statistic           = "Sum"
+  threshold           = "80"
+  alarm_description   = "This Alarm monitors if there are changes in the route tables of any VPC"
+}
+
+# Alarm to detect changes in Cloudtrail
+resource "aws_cloudwatch_metric_alarm" "aws_cloudtrail_changes" {
+  alarm_name          = "${var.name_prefix}-aws-cloudtrail-changes-alarm${var.name_suffix}"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = "1"
+  metric_name         = aws_cloudwatch_log_metric_filter.aws_cloudtrail_changes.metric_transformation[0].name
+  namespace           = aws_cloudwatch_log_metric_filter.aws_cloudtrail_changes.metric_transformation[0].namespace
+  period              = "60"
+  statistic           = "Sum"
+  threshold           = "80"
+  alarm_description   = "This Alarm monitors if there are changes in the AWS Cloudtrail configuration"
+}
+
+# Alarm to detect changes in EC2 instance status
+resource "aws_cloudwatch_metric_alarm" "ec2_instance_status_changes" {
+  alarm_name          = "${var.name_prefix}-ec2-instance-status-changes-alarm${var.name_suffix}"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = "1"
+  metric_name         = aws_cloudwatch_log_metric_filter.ec2_instance_status_changes.metric_transformation[0].name
+  namespace           = aws_cloudwatch_log_metric_filter.ec2_instance_status_changes.metric_transformation[0].namespace
+  period              = "60"
+  statistic           = "Sum"
+  threshold           = "80"
+  alarm_description   = "This Alarm monitors if there are changes in the status of EC2 instances"
+}
+
+# Alarm to detect Large EC2 instance launched
+resource "aws_cloudwatch_metric_alarm" "launch_ec2_large_instances" {
+  alarm_name          = "${var.name_prefix}-launch-ec2-large-instance-alarm${var.name_suffix}"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = "1"
+  metric_name         = aws_cloudwatch_log_metric_filter.launch_ec2_large_instances.metric_transformation[0].name
+  namespace           = aws_cloudwatch_log_metric_filter.launch_ec2_large_instances.metric_transformation[0].namespace
+  period              = "60"
+  statistic           = "Sum"
+  threshold           = "80"
+  alarm_description   = "This Alarm monitors if there are any large EC2 instances launched recently"
+}
+
+# Alarm to detect S3 bucket config changes
+resource "aws_cloudwatch_metric_alarm" "s3_bucket_config_changes" {
+  alarm_name          = "${var.name_prefix}-s3-bucket-config-changes-alarm${var.name_suffix}"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = "1"
+  metric_name         = aws_cloudwatch_log_metric_filter.s3_bucket_config_changes.metric_transformation[0].name
+  namespace           = aws_cloudwatch_log_metric_filter.s3_bucket_config_changes.metric_transformation[0].namespace
+  period              = "60"
+  statistic           = "Sum"
+  threshold           = "80"
+  alarm_description   = "This Alarm monitors if there are any configuration changes in any S3 bucket"
+}
+
+# Alarm to detect KMS CMK (Customer Managed Keys) configuration changes
+resource "aws_cloudwatch_metric_alarm" "aws_cmk_changes" {
+  alarm_name          = "${var.name_prefix}-aws-cmk-changes-alarm${var.name_suffix}"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = "1"
+  metric_name         = aws_cloudwatch_log_metric_filter.aws_cmk_changes.metric_transformation[0].name
+  namespace           = aws_cloudwatch_log_metric_filter.aws_cmk_changes.metric_transformation[0].namespace
+  period              = "60"
+  statistic           = "Sum"
+  threshold           = "80"
+  alarm_description   = "This Alarm monitors if there are any configuration changes in AWS KMS Customer Managed keys"
+}
---
+++ metrics.tf
@@ -0,0 +1,215 @@
+# Metrics related to authentication and IAM
+
+# Metric to detect Root account usage
+resource "aws_cloudwatch_log_metric_filter" "root_account_usage" {
+  name           = "RootAccountUsage"
+  pattern        = "{ $.userIdentity.type = \"Root\" && $.userIdentity.invokedBy NOT EXISTS && $.eventType != \"AwsServiceEvent\" }"
+  log_group_name = var.log_group_name
+
+  metric_transformation {
+    name          = "RootAccountUsageEventCount"
+    namespace     = "CloudTrailMetrics"
+    default_value = "0"
+    value         = "1"
+  }
+}
+
+# Metric to detect if any AWS console has been accessed by users without mfa
+resource "aws_cloudwatch_log_metric_filter" "console_sign_in_without_mfa" {
+  name           = "ConsoleSignInWithoutMfa"
+  pattern        = "{ $.eventName = \"ConsoleLogin\" && $.additionalEventData.MFAUsed = \"No\" }"
+  log_group_name = var.log_group_name
+
+  metric_transformation {
+    name          = "ConsoleSignInWithoutMfaCount"
+    namespace     = "CloudTrailMetrics"
+    default_value = "0"
+    value         = "1"
+  }
+}
+
+# Metric to detect if any AWS account is having authorization failures
+resource "aws_cloudwatch_log_metric_filter" "aws_authorization_failures" {
+  name           = "AWSAuthorizationFailures"
+  pattern        = "{ ($.errorCode = \"*UnauthorizedOperation\") || ($.errorCode = \"AccessDenied*\") }"
+  log_group_name = var.log_group_name
+
+  metric_transformation {
+    name          = "AuthorizationFailureCount"
+    namespace     = "CloudTrailMetrics"
+    default_value = "0"
+    value         = "1"
+  }
+}
+
+# Metric to detect any AWS with console sign in failures
+resource "aws_cloudwatch_log_metric_filter" "aws_console_sign_in_failures" {
+  name           = "AWSConsoleSignInFailures"
+  pattern        = "{ ($.eventName = ConsoleLogin) && ($.errorMessage = \"Failed authentication\") }"
+  log_group_name = var.log_group_name
+
+  metric_transformation {
+    name          = "ConsoleSigninFailureCount"
+    namespace     = "CloudTrailMetrics"
+    default_value = "0"
+    value         = "1"
+  }
+}
+
+# Metric to detect IAM auth config changes in your AWS accounts
+resource "aws_cloudwatch_log_metric_filter" "iam_auth_config_changes" {
+  name           = "IAMAuthConfigChanges"
+  pattern        = "{ ($.eventName = DeleteGroupPolicy) || ($.eventName = DeleteRolePolicy) || ($.eventName = DeleteUserPolicy) || ($.eventName = PutGroupPolicy) || ($.eventName = PutRolePolicy) || ($.eventName = PutUserPolicy) || ($.eventName = CreatePolicy) || ($.eventName = DeletePolicy) || ($.eventName = CreatePolicyVersion) || ($.eventName = DeletePolicyVersion) || ($.eventName = AttachRolePolicy) || ($.eventName = DetachRolePolicy) || ($.eventName = AttachUserPolicy) || ($.eventName = DetachUserPolicy) || ($.eventName = AttachGroupPolicy) || ($.eventName = DetachGroupPolicy) }"
+  log_group_name = var.log_group_name
+
+  metric_transformation {
+    name          = "IAMPolicyEventCount"
+    namespace     = "CloudTrailMetrics"
+    default_value = "0"
+    value         = "1"
+  }
+}
+
+# Metric to detect changes in AWS Organizations
+resource "aws_cloudwatch_log_metric_filter" "organizations_changes" {
+  name           = "OrganizationsChanges"
+  pattern        = "{ ($.eventSource = organizations.amazonaws.com) && ($.eventName = AcceptHandshake) || ($.eventName = AttachPolicy) || ($.eventName = CancelHandshake) || ($.eventName = CreateAccount) || ($.eventName = CreateOrganization) || ($.eventName = CreateOrganizationalUnit) || ($.eventName = CreatePolicy) || ($.eventName = DeclineHandshake) || ($.eventName = DeleteOrganization) || ($.eventName = DeleteOrganizationalUnit) || ($.eventName = DeletePolicy) || ($.eventName = EnableAllFeatures) || ($.eventName = EnablePolicyType) || ($.eventName = InviteAccountToOrganization) || ($.eventName = LeaveOrganization) || ($.eventName = DetachPolicy) || ($.eventName = DisablePolicyType) || ($.eventName = MoveAccount) || ($.eventName = RemoveAccountFromOrganization) || ($.eventName = UpdateOrganizationalUnit) || ($.eventName = UpdatePolicy) }"
+  log_group_name = var.log_group_name
+
+  metric_transformation {
+    name          = "OrganizationsEvents"
+    namespace     = "CloudTrailMetrics"
+    default_value = "0"
+    value         = "1"
+  }
+}
+
+# Metrics related to VCPs and networking
+
+# Metric to detect any AWS with console sign in failures
+resource "aws_cloudwatch_log_metric_filter" "vpc_gateway_config_changes" {
+  name           = "VPCGatewayConfigChanges"
+  pattern        = "{ ($.eventName = CreateCustomerGateway) || ($.eventName = DeleteCustomerGateway) || ($.eventName = AttachInternetGateway) || ($.eventName = CreateInternetGateway) || ($.eventName = DeleteInternetGateway) || ($.eventName = DetachInternetGateway) }"
+  log_group_name = var.log_group_name
+
+  metric_transformation {
+    name          = "GatewayEventCount"
+    namespace     = "CloudTrailMetrics"
+    default_value = "0"
+    value         = "1"
+  }
+}
+
+# Metric to detect changes in Security Groups
+resource "aws_cloudwatch_log_metric_filter" "security_group_config_changes" {
+  name           = "SecurityGroupConfigChanges"
+  pattern        = "{ ($.eventName = AuthorizeSecurityGroupIngress) || ($.eventName = AuthorizeSecurityGroupEgress) || ($.eventName = RevokeSecurityGroupIngress) || ($.eventName = RevokeSecurityGroupEgress) || ($.eventName = CreateSecurityGroup) || ($.eventName = DeleteSecurityGroup) }"
+  log_group_name = var.log_group_name
+
+  metric_transformation {
+    name          = "SecurityGroupEventCount"
+    namespace     = "CloudTrailMetrics"
+    default_value = "0"
+    value         = "1"
+  }
+}
+
+# Metric to detect changes in VPCs
+resource "aws_cloudwatch_log_metric_filter" "vpc_network_config_changes" {
+  name           = "VPCNetworkConfigChanges"
+  pattern        = "{ ($.eventName = CreateVpc) || ($.eventName = DeleteVpc) || ($.eventName = ModifyVpcAttribute) || ($.eventName = AcceptVpcPeeringConnection) || ($.eventName = CreateVpcPeeringConnection) || ($.eventName = DeleteVpcPeeringConnection) || ($.eventName = RejectVpcPeeringConnection) || ($.eventName = AttachClassicLinkVpc) || ($.eventName = DetachClassicLinkVpc) || ($.eventName = DisableVpcClassicLink) || ($.eventName = EnableVpcClassicLink) }"
+  log_group_name = var.log_group_name
+
+  metric_transformation {
+    name          = "VpcEventCount"
+    namespace     = "CloudTrailMetrics"
+    default_value = "0"
+    value         = "1"
+  }
+}
+
+# Metric to detect changes in Route tables
+resource "aws_cloudwatch_log_metric_filter" "route_table_config_changes" {
+  name           = "RouteTableConfigChanges"
+  pattern        = "{ ($.eventName = CreateRoute) || ($.eventName = CreateRouteTable) || ($.eventName = ReplaceRoute) || ($.eventName = ReplaceRouteTableAssociation) || ($.eventName = DeleteRouteTable) || ($.eventName = DeleteRoute) || ($.eventName = DisassociateRouteTable) }"
+  log_group_name = var.log_group_name
+
+  metric_transformation {
+    name          = "RouteTableEventCount"
+    namespace     = "CloudTrailMetrics"
+    default_value = "0"
+    value         = "1"
+  }
+}
+
+# Metrics related to other services
+
+# Metric to detect any change in AWS Cloudtrail
+resource "aws_cloudwatch_log_metric_filter" "aws_cloudtrail_changes" {
+  name           = "AWSCloudTrailChanges"
+  pattern        = "{ ($.eventName = CreateTrail) || ($.eventName = UpdateTrail) || ($.eventName = DeleteTrail) || ($.eventName = StartLogging) || ($.eventName = StopLogging) }"
+  log_group_name = var.log_group_name
+
+  metric_transformation {
+    name          = "CloudTrailEventCount"
+    namespace     = "CloudTrailMetrics"
+    default_value = "0"
+    value         = "1"
+  }
+}
+
+# Metric to detect any change in EC2 Instances
+resource "aws_cloudwatch_log_metric_filter" "ec2_instance_status_changes" {
+  name           = "EC2InstanceStatusChanges"
+  pattern        = "{ ($.eventName = RunInstances) || ($.eventName = RebootInstances) || ($.eventName = StartInstances) || ($.eventName = StopInstances) || ($.eventName = TerminateInstances) }"
+  log_group_name = var.log_group_name
+
+  metric_transformation {
+    name          = "EC2InstanceEventCount"
+    namespace     = "CloudTrailMetrics"
+    default_value = "0"
+    value         = "1"
+  }
+}
+
+# Metric to detect if large EC2 instances have been launched
+resource "aws_cloudwatch_log_metric_filter" "launch_ec2_large_instances" {
+  name           = "LaunchEC2LargeInstances"
+  pattern        = "{ ($.eventName = RunInstances) && (($.requestParameters.instanceType = *.8xlarge) || ($.requestParameters.instanceType = *.4xlarge)) }"
+  log_group_name = var.log_group_name
+
+  metric_transformation {
+    name          = "EC2LargeInstanceEventCount"
+    namespace     = "CloudTrailMetrics"
+    default_value = "0"
+    value         = "1"
+  }
+}
+
+# Metric to detect config changes in S3 buckets
+resource "aws_cloudwatch_log_metric_filter" "s3_bucket_config_changes" {
+  name           = "S3BucketConfigChanges"
+  pattern        = "{ ($.eventSource = s3.amazonaws.com) && (($.eventName = PutBucketAcl) || ($.eventName = PutBucketPolicy) || ($.eventName = PutBucketCors) || ($.eventName = PutBucketLifecycle) || ($.eventName = PutBucketReplication) || ($.eventName = DeleteBucketPolicy) || ($.eventName = DeleteBucketCors) || ($.eventName = DeleteBucketLifecycle) || ($.eventName = DeleteBucketReplication)) }"
+  log_group_name = var.log_group_name
+
+  metric_transformation {
+    name          = "S3BucketEventCount"
+    namespace     = "CloudTrailMetrics"
+    default_value = "0"
+    value         = "1"
+  }
+}
+
+# Metric to detect if there are changes in Customer Managed Keys
+resource "aws_cloudwatch_log_metric_filter" "aws_cmk_changes" {
+  name           = "AWSCMKChanges"
+  pattern        = "{ ($.eventSource = kms.amazonaws.com) && (($.eventName = DisableKey) || ($.eventName = ScheduleKeyDeletion)) }"
+  log_group_name = var.log_group_name
+
+  metric_transformation {
+    name          = "CMKEventCount"
+    namespace     = "CloudTrailMetrics"
+    default_value = "0"
+    value         = "1"
+  }
+}
