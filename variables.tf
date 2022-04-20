df0d44c013bb74eaefa9afdde24b5572f48c91ce
Angel Lopez <78411884+angellopez-sg@users.noreply.github.com>
2022-04-20 11:14:24 +0200

    Adding KMS and SNS resources (#1)
    * Adding KMS and SNS resources
    
    Signed-off-by: Angel Lopez <angel.lopez@stratusgrid.com>
    
    * Adding last line in data and variables
    
    Signed-off-by: Angel Lopez <angel.lopez@stratusgrid.com>

--- variables.tf
+++ variables.tf
@@ -1,5 +1,12 @@
-variable "name" {
-  description = "name to prepend to all resource names within module"
+# General variables
+variable "name_prefix" {
+  description = "Name to prepend to all resource names within module"
+  type        = string
+}
+
+variable "name_suffix" {
+  description = "Name to append to all resource names within module"
+  default     = ""
   type        = string
 }
 
@@ -7,7 +14,26 @@ variable "input_tags" {
   description = "Map of tags to apply to resources"
   type        = map(string)
   default = {
-    Developer   = "StratusGrid"
-    Provisioner = "Terraform"
+    ModuleDeveloper = "StratusGrid"
+    Provisioner     = "Terraform"
   }
+}
+
+# Variables related to Cloudwatch
+variable "log_group_name" {
+  description = "Name of the CloudWatch Log Group where the events are being recorded"
+  type        = string
+}
+
+# Variables related to KMS
+variable "create_kms_key" {
+  description = "Boolean to define if KMS key should be created or not"
+  default     = true
+  type        = bool
+}
+
+variable "kms_key_id" {
+  description = "ID of the key to be used by Cloud Trail to encrypt the logs"
+  default     = ""
+  type        = string
 }
